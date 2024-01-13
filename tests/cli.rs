b95cf61118b2f413fcc827585ad5370acf949bdc
Renato Diniz <renatodiniz25@gmail.com>
2024-01-13 09:40:20 -0300

    tests: add more specs (#3)

--- tests/cli.rs
+++ tests/cli.rs
@@ -1,6 +1,5 @@
 use assert_cmd::Command;
 
-// 123 45 3 28 74 19123 28 28 1
 #[test]
 fn assert_bubble_sort() {
     let mut cmd = Command::cargo_bin("dsa_rust").unwrap();
@@ -10,6 +9,17 @@ fn assert_bubble_sort() {
     cmd.assert()
         .success()
         .stdout("[1, 3, 28, 28, 28, 45, 74, 123, 19123]\n");
+}
+
+#[test]
+fn assert_bubble_sort2() {
+    let mut cmd = Command::cargo_bin("dsa_rust").unwrap();
+
+    cmd.args(["-b", "1"]);
+
+    cmd.assert()
+        .success()
+        .stdout("[1]\n");
 }
 
 #[test]
@@ -24,6 +34,17 @@ fn assert_selection_sort() {
 }
 
 #[test]
+fn assert_selection_sort2() {
+    let mut cmd = Command::cargo_bin("dsa_rust").unwrap();
+
+    cmd.args(["-s", "1"]);
+
+    cmd.assert()
+        .success()
+        .stdout("[1]\n");
+}
+
+#[test]
 fn assert_insertion_sort() {
     let mut cmd = Command::cargo_bin("dsa_rust").unwrap();
 
@@ -32,4 +53,15 @@ fn assert_insertion_sort() {
     cmd.assert()
         .success()
         .stdout("[1, 3, 28, 28, 28, 45, 74, 123, 19123]\n");
+}
+
+#[test]
+fn assert_insertion_sort2() {
+    let mut cmd = Command::cargo_bin("dsa_rust").unwrap();
+
+    cmd.args(["-i", "1"]);
+
+    cmd.assert()
+        .success()
+        .stdout("[1]\n");
 }
