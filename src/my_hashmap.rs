70982d1113c6e11ebb74b5849550b12eba186190
Renato Diniz <renatodiniz25@gmail.com>
2024-01-15 10:32:52 -0300

    refactor: use arrays intead of vector for myhash implementation

--- src/my_hashmap.rs
+++ src/my_hashmap.rs
@@ -2,31 +2,26 @@ use std::collections::hash_map::DefaultHasher;
 use std::hash::{Hash, Hasher};
 
 // ## Note
-// The implementation is theoretical and may lead to memory overflow
-// due to excessive vector lengths.
+// The implementation is theoretical and may lead to poor developer experience
+// and memory optimization.
 // It is advisable to consider alternative data structures
 // or resizing strategies for practical use.
 
-pub fn create_hash() -> Vec<usize> {
-    let vec: Vec<usize> = Vec::new();
-    vec
-}
-
-pub fn insert_into_my_hashmap(mut hsh: Vec<usize>, key: String, value: usize) -> Vec<usize> {
-    let index_to_insert = hash_key(&key);
-
-    if index_to_insert > hsh.len() {
-        hsh.resize(index_to_insert + 1, 0); // Resize and fill with default value (0 in this case)
-    }
-    hsh.insert(index_to_insert, value);
-
+pub fn create_hash() -> [u32; 500] {
+    let hsh = [0; 500];
     hsh
 }
 
-pub fn get_from_my_hashmap(hsh: Vec<usize>, key: String) -> usize {
-    let index_to_search = hash_key(&key);
+pub fn insert_into_my_hashmap(hsh: &mut [u32; 500], key: &str, value: u32) {
+    let index_to_insert = hash_key(&key) % hsh.len();
 
-    *hsh.get(index_to_search).unwrap()
+    hsh[index_to_insert] = value;
+}
+
+pub fn get_from_my_hashmap(hsh: &[u32; 500], key: &str) -> u32 {
+    let index_to_search = hash_key(&key) % hsh.len();
+
+    hsh[index_to_search]
 }
 
 fn hash_key(key: &str) -> usize {
