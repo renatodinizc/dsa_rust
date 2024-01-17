52167c2b83f8e4ae6ef97e488705caf9dd8f2a5e
Renato Diniz <renatodiniz25@gmail.com>
2024-01-16 22:39:51 -0300

    refactor: refact algorithm implementations

--- src/bubble_sort.rs
+++ src/bubble_sort.rs
@@ -2,18 +2,12 @@ pub fn sort(mut numbers: Vec<u32>) {
     let mut done: bool = false;
 
     while !done {
-        let mut i = 0;
-        let mut j = 1;
         done = true;
-
-        while j < numbers.len() {
+        for (i, j) in (1..numbers.len()).enumerate() {
             if numbers[i] > numbers[j] {
                 numbers.swap(i, j);
                 done = false;
             }
-
-            i += 1;
-            j += 1;
         }
     }
 
--- src/selection_sort.rs
+++ src/selection_sort.rs
@@ -1,19 +1,13 @@
 pub fn sort(mut numbers: Vec<u32>) {
     let mut lowest_number_index = 0;
-    let mut i = 0;
 
-    while i < numbers.len() {
-        let mut j = i;
-
-        while j < numbers.len() {
+    for i in 0..numbers.len() {
+        for j in i..numbers.len() {
             if numbers[j] < numbers[lowest_number_index] {
                 lowest_number_index = j;
             }
-            j += 1;
         }
-
         numbers.swap(i, lowest_number_index);
-        i += 1;
     }
 
     println!("{:?}", numbers);
