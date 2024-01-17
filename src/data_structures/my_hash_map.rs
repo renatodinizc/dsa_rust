20db12dbe65953ff1fe42a8412799e61fbc7b96b
Renato Diniz <renatodiniz25@gmail.com>
2024-01-17 17:10:28 -0300

    refactor: improve hashmap design

--- src/data_structures/my_hash_map.rs
+++ src/data_structures/my_hash_map.rs
@@ -1,37 +1,41 @@
 use std::collections::hash_map::DefaultHasher;
 use std::hash::{Hash, Hasher};
 
+#[derive(Debug)]
 pub struct MyHashMap<K, V> {
-    arr: [Option<(K, V)>; 600],
+    vec: Vec<Option<(K, V)>>,
 }
 
-impl<K: Copy + Hash, V: Copy> MyHashMap<K, V> {
+impl<K: Clone + Hash, V: Clone> MyHashMap<K, V> {
     pub fn new() -> MyHashMap<K, V> {
-        MyHashMap { arr: [None; 600] }
+        MyHashMap { vec: vec![None; 500] }
     }
 
-    pub fn insert(&mut self, key: K, value: V) {
-        let key_value = (key, value);
+    pub fn insert(&mut self, key: K, value: V) -> Option<V> {
+        let index = Self::hash_key(&key) % self.len() as u64;
 
-        let index = Self::hash_key(key) % self.len() as u64;
+        self.vec[index as usize] = Some((key, value.clone()));
 
-        self.arr[index as usize] = Some(key_value);
+        Some(value)
     }
 
-    pub fn get(&self, key: K) -> Option<V> {
-        let index = Self::hash_key(key) % self.len() as u64;
+    pub fn get(&self, key: K) -> Option<&V> {
+        let index = Self::hash_key(&key) % self.len() as u64;
 
-        match self.arr[index as usize] {
-            Some(key_value) => Some(key_value.1),
+        match self.vec.get(index as usize) {
+            Some(key_value_option) => match key_value_option {
+                Some((_key, value)) => Some(value),
+                None => None,
+            },
             None => None,
         }
     }
 
     pub fn remove(&mut self, key: K) -> Option<V> {
-        let index = Self::hash_key(key) % self.arr.len() as u64;
+        let index = Self::hash_key(&key) % self.vec.len() as u64;
 
-        let removed_value = self.arr[index as usize];
-        self.arr[index as usize] = None;
+        let removed_value = self.vec.remove(index as usize);
+        self.vec.insert(index as usize, None);
 
         match removed_value {
             Some(key_value) => Some(key_value.1),
@@ -40,10 +44,10 @@ impl<K: Copy + Hash, V: Copy> MyHashMap<K, V> {
     }
 
     fn len(&self) -> usize {
-        self.arr.len()
+        self.vec.len()
     }
 
-    fn hash_key(key: K) -> u64 {
+    fn hash_key(key: &K) -> u64 {
         let mut hasher = DefaultHasher::new();
         key.hash(&mut hasher);
 
@@ -59,45 +63,60 @@ impl<K: Copy + Hash, V: Copy> Default for MyHashMap<K, V> {
 
 #[test]
 fn assert_hashmap_get_item() {
-    let mut dict: MyHashMap<&str, u32> = MyHashMap::new();
+    let mut dict: MyHashMap<String, u32> = MyHashMap::new();
 
-    dict.insert("banana", 10);
+    dict.insert(String::from("banana"), 7880);
 
-    assert_eq!(dict.get("banana"), Some(10));
-    assert_eq!(dict.get("orange"), None);
+    assert_eq!(dict.get(String::from("banana")), Some(&7880));
+    assert_eq!(dict.get(String::from("orange")), None);
 }
 
 #[test]
 fn assert_hashmap_remove_item() {
-    let mut dict: MyHashMap<&str, u32> = MyHashMap::new();
+    let mut dict: MyHashMap<String, u32> = MyHashMap::new();
 
-    dict.insert("banana", 10);
+    dict.insert("banana".to_string(), 10);
 
-    assert_eq!(dict.remove("banana"), Some(10));
-    assert_eq!(dict.remove("orange"), None);
+    assert_eq!(dict.remove(String::from("banana")), Some(10));
+    assert_eq!(dict.remove(String::from("banana")), None);
 }
 
 #[test]
 fn assert_hashmap_update_item() {
-    let mut dict: MyHashMap<&str, u8> = MyHashMap::new();
+    let mut dict: MyHashMap<String, u32> = MyHashMap::new();
 
-    dict.insert("watermelon", 3);
-    dict.insert("watermelon", 18);
+    dict.insert("watermelon".to_string(), 3);
+    dict.insert("watermelon".to_string(), 18);
 
-    assert_eq!(dict.get("watermelon"), Some(18));
+    assert_eq!(dict.get(String::from("watermelon")), Some(&18));
 }
 
 #[test]
-fn assert_hashmap_insert_multiple_items() {
-    let mut dict: MyHashMap<&str, &str> = MyHashMap::new();
+fn assert_hashmap_get_multiple_key_value_types() {
+    let mut dict1: MyHashMap<String, u8> = MyHashMap::new();
+    let mut dict2: MyHashMap<u64, &str> = MyHashMap::new();
+    let mut dict3: MyHashMap<u8, String> = MyHashMap::new();
 
-    dict.insert("watermelon", "red");
-    dict.insert("potato", "yellow");
-    dict.insert("orange", "orange");
-    dict.insert("grape", "purple");
+    dict1.insert(String::from("banana"), 17);
+    dict2.insert(778, "plane");
+    dict3.insert(10, String::from("lover"));
 
-    assert_eq!(dict.get("watermelon"), Some("red"));
-    assert_eq!(dict.get("potato"), Some("yellow"));
-    assert_eq!(dict.get("orange"), Some("orange"));
-    assert_eq!(dict.get("grape"), Some("purple"));
+    assert_eq!(dict1.get(String::from("banana")), Some(&17));
+    assert_eq!(dict2.get(778), Some(&"plane"));
+    assert_eq!(dict3.get(10u8), Some(&String::from("lover")));
+}
+
+#[test]
+fn assert_hashmap_remove_multiple_key_value_types() {
+    let mut dict1: MyHashMap<String, u8> = MyHashMap::new();
+    let mut dict2: MyHashMap<u64, &str> = MyHashMap::new();
+    let mut dict3: MyHashMap<u8, String> = MyHashMap::new();
+
+    dict1.insert(String::from("banana"), 17);
+    dict2.insert(778, "plane");
+    dict3.insert(10, String::from("lover"));
+
+    assert_eq!(dict1.remove(String::from("banana")), Some(17));
+    assert_eq!(dict2.remove(778), Some("plane"));
+    assert_eq!(dict3.remove(10u8), Some(String::from("lover")));
 }
