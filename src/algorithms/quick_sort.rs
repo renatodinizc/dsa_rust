e4502db987cd8314bfd7b57f2cbdef2696e5dbd5
Renato Diniz <renatodiniz25@gmail.com>
2024-01-18 21:09:24 -0300

    tests: improve specs

--- src/algorithms/quick_sort.rs
+++ src/algorithms/quick_sort.rs
@@ -42,104 +42,47 @@ fn partition<T: PartialOrd + Clone>(numbers: &mut [T]) -> usize {
 
 #[cfg(test)]
 mod tests {
-    use super::{partition, sort};
+    use super::sort;
 
     #[test]
-    fn assert_quick_sort_with_u32() {
-        let numbers = vec![123u32, 45, 3, 28, 74, 19123, 28, 28, 1];
-
-        let result = sort(numbers);
-
-        assert_eq!(result, [1, 3, 28, 28, 28, 45, 74, 123, 19123]);
+    fn sort_numbers() {
+        let input = vec![4, 2, 3, 1, 5];
+        assert_eq!(sort(input), vec![1, 2, 3, 4, 5]);
     }
 
     #[test]
-    fn assert_quick_sort_with_f64() {
-        let numbers = vec![
-            123.4, 45f64, 3.1, 28.1, 28.02, 19123.97, 28.01, 28.001, 1f64,
-        ];
-
-        let result = sort(numbers);
-
-        assert_eq!(
-            result,
-            [1f64, 3.1, 28.001, 28.01, 28.02, 28.1, 45f64, 123.4, 19123.97]
-        );
+    fn sort_empty_vec() {
+        let empty_vec: Vec<i32> = vec![];
+        assert_eq!(sort(empty_vec), vec![]);
     }
 
     #[test]
-    fn assert_quick_sort_with_str() {
-        let numbers = vec!["grapes", "potatoes", "apples", "pineapples", "watermelons"];
-
-        let result = sort(numbers);
-
-        assert_eq!(
-            result,
-            ["apples", "grapes", "pineapples", "potatoes", "watermelons"]
-        );
+    fn sort_single_element_vec() {
+        let single_element = vec![1];
+        assert_eq!(sort(single_element), vec![1]);
     }
 
     #[test]
-    fn assert_quick_sort_with_string() {
-        let numbers = vec![
-            "grapes".to_string(),
-            "potatoes".to_string(),
-            "apples".to_string(),
-            "pineapples".to_string(),
-            "watermelons".to_string(),
-        ];
-
-        let result = sort(numbers);
-
-        assert_eq!(
-            result,
-            [
-                "apples".to_string(),
-                "grapes".to_string(),
-                "pineapples".to_string(),
-                "potatoes".to_string(),
-                "watermelons".to_string()
-            ]
-        );
+    fn sort_characters() {
+        let chars = vec!['c', 'a', 'b', 'e', 'd'];
+        assert_eq!(sort(chars), vec!['a', 'b', 'c', 'd', 'e']);
     }
 
     #[test]
-    fn assert_partition_with_u32() {
-        let mut numbers = vec![0u32, 5, 2, 1, 6, 3];
-
-        let result = partition(&mut numbers);
-        assert_eq!(result, 3);
+    fn sort_strs() {
+        let strs = vec!["beta", "alpha", "gamma"];
+        assert_eq!(sort(strs), vec!["alpha", "beta", "gamma"]);
     }
 
     #[test]
-    fn assert_partition_with_f64() {
-        let mut numbers = vec![0f64, 5.1, 2.001, 1.5, 6.3, 3f64];
-
-        let result = partition(&mut numbers);
-        assert_eq!(result, 3);
+    fn sort_strings() {
+        let strings = vec!["beta".to_string(), "alpha".to_string(), "gamma".to_string()];
+        assert_eq!(sort(strings), vec!["alpha", "beta", "gamma"]);
     }
 
     #[test]
-    fn assert_partition_with_str() {
-        let mut numbers = vec!["carrot", "apple", "grape", "banana", "zucchini", "lemon"];
-
-        let result = partition(&mut numbers);
-        assert_eq!(result, 4);
-    }
-
-    #[test]
-    fn assert_partition_with_string() {
-        let mut numbers = vec![
-            "carrot".to_string(),
-            "apple".to_string(),
-            "grape".to_string(),
-            "banana".to_string(),
-            "zucchini".to_string(),
-            "lemon".to_string(),
-        ];
-
-        let result = partition(&mut numbers);
-
-        assert_eq!(result, 4);
+    fn sort_already_quick_sorted() {
+        let input = vec![1, 2, 3, 4, 5];
+        assert_eq!(sort(input.clone()), input);
     }
 }
